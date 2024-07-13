cbc963d70643ba1205aacd682d89133e5e32fe1a
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2024-07-13 15:40:12 +0200

    navbar & hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { getImageUrl } from '../../utils'
+import ReactPlayer from 'react-player'
+
 import styles from './Hero.module.css'
 
 export const Hero = () => {
@@ -8,10 +9,9 @@ export const Hero = () => {
             <div className={styles.content}>
                 <h1 className={styles.title}>Hi, I'm Gwenaëlle</h1>
                 <p className={styles.description}>I'm a fullstack developper</p>
-                <a className={styles.contactBtn} href="mailto:cgenaelle@hotmail.com">Contactez-moi !</a></div>
-            <img className={styles.heroImg}src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" />
-            <div className={styles.topBlur} />
-            <div className={styles.bottomBlur} />
+                <a className={styles.contactBtn} href="mailto:cgenaelle@hotmail.com">Contactez-moi !</a>
+            </div>
+            <ReactPlayer url="/assets/hero/Gwenaelle_Bousses.mp4" controls />
         </section>
     )
 }
