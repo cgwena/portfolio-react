9b3b482ae075a2990a57ad46af476a9ded10e3d3
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2024-07-25 19:22:44 +0200

    hero and about sections

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,32 +1,13 @@
 import React from 'react'
-import { getImageUrl } from '../../utils'
+import ReactPlayer from 'react-player'
+
 import styles from "./About.module.css"
 
 export const About = () => {
     return (
-        <section className={styles.container}>
-            <div className={styles.content}>
-                <div>                
-                    <img className={styles.aboutImg} src={getImageUrl("about/Gwenaelle_Bousses.jpg")} alt="Me sitting with a laptop" />
-                </div>
-                <ul className={styles.aboutItems}>
-                    <li className={styles.aboutItem}>
-                        <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
-                        <div className={styles.aboutItemText}>
-                            <h3>Développeuse Front-End</h3>
-                            <p>Je suis développeuse Front-End et je construis des sites optimisés et adaptés à tous les formats.</p>
-                        </div>
-                    </li>
-                    <li className={styles.aboutItem}>
-                        <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
-                        <div className={styles.aboutItemText}>
-                            <h3>Développeuse Back-End</h3>
-                            <p>Je sais construire et gérer des bases de données et des APIs sécurisées.</p>
-                        </div>
-                    </li>
-                    
-                </ul>
-            </div>
+        <section className={styles.container} id='about'>
+            <h2 className={styles.title}>Pour mieux me connaître...</h2>
+            <ReactPlayer url="/assets/hero/Gwenaelle_Bousses.mp4" controls />
         </section>
     )
 }
