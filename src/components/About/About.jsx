2b9c0fc386521f024abe57cdf7ee6124cd7069d8
Gwenaëlle Boussès <cgwenaelle@hotmail.com>
2025-02-12 17:03:43 +0100

    Photo updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,7 +8,7 @@ export const About = () => {
         <section className={styles.container}>
             <div className={styles.content}>
                 <div>                
-                    <img className={styles.aboutImg} src={getImageUrl("about/Gwenaelle_Bousses.jpg")} alt="Me sitting with a laptop" />
+                    <img className={styles.aboutImg} src={getImageUrl("about/photo_rose.png")} alt="Me sitting with a laptop" />
                 </div>
                 <ul className={styles.aboutItems}>
                     <li className={styles.aboutItem}>
